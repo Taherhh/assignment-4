41de61ff0ece49f5e8a03a5cacd472f3473188ae
Taher Shabbir <taherihh53@gmail.com>
2024-06-09 03:12:15 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,5 @@
+//assignment no 4
+let famQoute:string = "A person who never made a mistake never tried anything new.";
+let author:string ="Albert Einstein";
+
+console.log(`${author} once said, "${famQoute}"`);
